75a776859ddd7bc05a4b907b517cebb65a7628bb
danvangogh <djameswhite@yahoo.com>
2018-10-30 02:04:30 +0000

    added access token to secret file and added to gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,17 @@
 var request = require('request');
+var tokens = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
