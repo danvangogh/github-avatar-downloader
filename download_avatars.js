83fc444e98eedb226a0384bf5da551b5513ee2ac
danvangogh <djameswhite@yahoo.com>
2018-10-30 04:59:57 +0000

    step 10 - linked functions and added error message. Not sure if working like it should.

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,10 @@
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
+// var url = process.argv[2];
+// var filePath = process.argv[3];
 
-function getRepoContributors(repoOwner, repoName, handleResponse) {
+function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -10,8 +12,12 @@ function getRepoContributors(repoOwner, repoName, handleResponse) {
       'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
+  if (!process.argv[2] || !process.argv[3]) {
+    console.log("gimme more info!!!");
+    return
+  }
 
-  request(options, handleResponse);
+  request(options, cb);
 }
 
 getRepoContributors("jquery", "jquery", function(error, response) {
@@ -19,13 +25,17 @@ getRepoContributors("jquery", "jquery", function(error, response) {
 
   contributors.forEach(function (contributor) {
     console.log(contributor.avatar_url);
+    downloadImageByURL(process.argv[2], process.argv[3]);
   })
 });
+
+
+
 
 function downloadImageByURL(url, filePath) {
 
 request.get(url)               // Note 1
-       .on('error', function (error) {                                   // Note 2
+       .on('error', function (err) {                                   // Note 2
          throw err;
        })
        .on('response', function (response) {                           // Note 3
@@ -34,4 +44,4 @@ request.get(url)               // Note 1
        .pipe(fs.createWriteStream(filePath));               // Note 4
 };
 
-downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
+// downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
